782f629621f124ca8db0dc4e143f39dc217f64f0
irfan-rumon <irfannisho8571@gmail.com>
2022-09-15 19:45:51 +0600

    Initialize project folders

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { ProductItemComponent } from './components/product-item/product-item.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    ProductItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/models/product.ts
@@ -0,0 +1,8 @@
+export interface Product {
+    id: number;
+    imageURL: string;
+    name: string;
+    unitPrice : number;
+    quantity: number;
+    catagory:string;
+  }
