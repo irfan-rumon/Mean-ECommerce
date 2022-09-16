41632d34e632bc286df881c0a53a7e21480f58dc
irfan-rumon <irfannisho8571@gmail.com>
2022-09-16 10:30:02 +0600

    Add cart component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
 import { DisplayComponent } from './components/display/display.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
 
 const routes: Routes = [
      {path: '',  component:  HomePageComponent },
-     {path: 'display', component: DisplayComponent}
+     {path: 'display', component: DisplayComponent},
+     {path: 'cart', component: CartComponent}
 ]
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,12 +7,16 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SliderComponent } from './components/slider/slider.component';
-
 import { CatagoryItemComponent } from './components/catagory-item/catagory-item.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { DisplayComponent } from './components/display/display.component';
 import { CheckboxComponent } from './components/checkbox/checkbox.component';
 import { CheckboxServicesOptionComponent } from './components/checkbox-services-option/checkbox-services-option.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { CartComponent } from './components/cart/cart.component';
+import { OrderComponent } from './components/order/order.component';
+
 
 @NgModule({
   declarations: [
@@ -25,7 +29,14 @@ import { CheckboxServicesOptionComponent } from './components/checkbox-services-
     FooterComponent,
     DisplayComponent,
     CheckboxComponent,
-    CheckboxServicesOptionComponent
+    CheckboxServicesOptionComponent,
+    LoginComponent,
+    RegisterComponent,
+    CartComponent,
+    OrderComponent,
+   
+    
+    
   ],
   imports: [
     BrowserModule,
