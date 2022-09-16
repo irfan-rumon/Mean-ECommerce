fe059ba21a66f11c8acc2734e49bf08b5cd7b573
irfan-rumon <irfannisho8571@gmail.com>
2022-09-16 14:25:13 +0600

    Add sign up component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './components/cart/cart.component';
 import { DisplayComponent } from './components/display/display.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
      {path: '',  component:  HomePageComponent },
      {path: 'display', component: DisplayComponent},
-     {path: 'cart', component: CartComponent}
+     {path: 'cart', component: CartComponent},
+     {path: 'login', component: LoginComponent},
+     {path: 'register', component: RegisterComponent}
 ]
 
 @NgModule({
