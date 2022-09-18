49378a6bc1ead19d35ef8638a863733fa0e6c3cc
irfan-rumon <irfannisho8571@gmail.com>
2022-09-18 11:39:56 +0600

    Add feature of displaying trending products in homepage

--- frontend/src/app/components/home-page/home-page.component.ts
+++ frontend/src/app/components/home-page/home-page.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { CatagoryApiService } from 'src/app/services/catagory-api.service';
 import { Router } from '@angular/router';
 import { Catagory } from 'src/app/models/catagory';
+import { Product } from 'src/app/models/product';
+import { ProductApiService } from 'src/app/services/product-api.service';
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -10,14 +13,30 @@ import { Catagory } from 'src/app/models/catagory';
 export class HomePageComponent implements OnInit {
 
   catagories: Catagory[] = [];
+  products: Product[];
+  trendingProducts: Product[] = []
+
   
-  constructor(private router: Router, private catagoryApi:CatagoryApiService) { }
+  constructor(private router: Router, private catagoryApi:CatagoryApiService,
+              private productApi: ProductApiService
+    ) { }
 
   ngOnInit(): void {
     this.catagoryApi.getCatagories().subscribe(  (catagories)=>{
        this.catagories = catagories;
        console.log("heyy", catagories);
     } )
+    this.productApi.getProducts().subscribe( (products)=>{
+         this.products = products;
+         for (let pr of this.products){
+            console.log("Trending Status:",  pr.isTrending);
+            console.log("Type of trending variable:", typeof pr.isTrending);
+            if( pr.isTrending == true)this.trendingProducts.push( pr );
+         }
+         //console.log("Products:", this.products);
+         console.log("Trending:", this.trendingProducts);
+    } )
+
   }
 
 }
