0e662105f21f28b9ec72411d2d0ae92bf5264a7d
irfan-rumon <irfannisho8571@gmail.com>
2022-09-26 16:12:20 +0600

    Add CRUD feature of products

--- backend/src/services/index.ts
+++ backend/src/services/index.ts
@@ -2,10 +2,12 @@ import { Application } from '../declarations';
 import users from './users/users.service';
 import cartProducts from './cart-products/cart-products.service';
 import orderProducts from './order-products/order-products.service';
+import products from './products/products.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
   app.configure(users);
   app.configure(cartProducts);
   app.configure(orderProducts);
+  app.configure(products);
 }
